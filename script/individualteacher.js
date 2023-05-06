16b986c916d0eaf0975edebf6b43f6ec189a9e93
vaishnavi0717 <vaishnavipunekar0@gmail.com>
2023-05-06 08:06:49 +0530

    day3

---
+++ script/individualteacher.js
@@ -0,0 +1,41 @@
+   
+    let teacherdata=JSON.parse(localStorage.getItem("teachers"))||[];
+    let disteacher=[]
+    
+    disteacher.push(teacherdata)
+        console.log(disteacher)
+    
+        let container=document.getElementById("individual-cont");
+        let imagdiv=document.getElementById("imgdiv")
+        detaildiv=document.getElementById("detaildiv")
+    
+        function displayData(data){
+        container.innerHTML=null; 
+    
+       data.forEach((element)=>{
+    
+    let teacher=document.createElement("div")
+    
+    let name=document.createElement("h2")
+    name.textContent=element.name;
+    
+    let image=document.createElement("img")
+    image.setAttribute("src", element.avatar)
+    
+    let title=document.createElement("h5")
+    title.textContent=element.title;
+    
+    let discription=document.createElement("p")
+    discription.textContent=element.description
+
+
+    container.append(teacher)
+    teacher.append(imagdiv, detaildiv)
+    imagdiv.append(image)
+    detaildiv.append(name, title, discription)
+    
+       })
+    
+    }
+    
+    displayData(disteacher)
